512917dd5d5fa39c6e00d9377209976f23d5b1e9
Roycute <86759430+Roycute@users.noreply.github.com>
2021-07-01 11:35:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 const CONFIG = {
-    introTitle: 'Con à!',
+    introTitle: 'Bông Bông. Con à!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Mà sao con cứ đi nhầm đường
+    Lạc vào tim chú lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    title: 'Phải chăng con đã yêu ngay từ cái lần đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
+    btnYes: 'Vẫn cứ là thích chú <33',
+    btnNo: 'Không, chú à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao con lại yêu chú baeee <3',
+    btnReply: 'Gửi chú nè <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Chú biết mà 🥰. Yêu con nhiều nhiều 😘😘',
     messDesc: 'Chú chờ con đó nhé công chúa.',
